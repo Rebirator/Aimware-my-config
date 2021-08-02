e3274843fb605b3e2768ff6272d70e49b411fff9
Rebirator <34403611+Rebirator@users.noreply.github.com>
2021-08-02 23:17:48 +0300

    Update Chieftain.lua

--- Chieftain.lua
+++ Chieftain.lua
@@ -495,7 +495,7 @@ end
 local function lag_on_peek()
 	local Player, LocalPlayer = entities_check()
 
-    if CHIEFTAIN_DOUBLEFIRE_MODE[WEAPON_CURRENT_GROUP][1]:GetValue() == 0 or CHIEFTAIN_DOUBLEFIRE_PERF_LAGPEEK[WEAPON_CURRENT_GROUP][1]:GetValue() == 0 then
+    if not CHIEFTAIN_DOUBLEFIRE_PERF_LAGPEEK[WEAPON_CURRENT_GROUP][1]:GetValue() then
         return
     end
     
