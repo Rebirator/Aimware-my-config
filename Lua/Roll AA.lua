aba6c948a5b1896803e528f8f51b3c70d98d2f44
Alexandr Orden <34403611+Rebirator@users.noreply.github.com>
2022-01-03 15:27:32 +0300

    Update Roll AA.lua

--- Lua/Roll AA.lua
+++ Lua/Roll AA.lua
@@ -7,14 +7,6 @@
 ------------------------------------------Credits-------------------------------------------
 
 local math_sqrt, bit_band, entities_GetLocalPlayer, gui_GetValue = math.sqrt, bit.band, entities.GetLocalPlayer, gui.GetValue;
-local WEAPONID_KNIFES = { 41, 42, 59, 69, 74, 75, 76, 78, 500, 503, 505, 506, 507, 508, 509, 512, 514, 515, 516, 517, 518, 519, 520, 521, 522, 523, 525 };
-local function LPWeaponID( weapon_id )
-    for k, v in pairs( weapon_id ) do
-        if entities_GetLocalPlayer( ):GetWeaponID( ) == weapon_id[ k ] then
-            return true;
-        end;
-    end;
-end;
 
 local g_bRollAA         = gui.Checkbox( gui.Reference("Ragebot", "Anti-Aim", "Extra" ), "rollaa", "◆ Enable Roll AA ◆", false );
 g_bRollAA:SetDescription( "Enabling extended desync degree" );
@@ -24,10 +16,6 @@ local g_iRollAAMovValue = gui.Slider( gui.Reference("Ragebot", "Anti-Aim", "Extr
 g_iRollAAMovValue:SetDescription( "The higher the value, the more incorrect the movement." );
 
 callbacks.Register( "CreateMove", "RollAA001", function( UserCmd )
-    if LPWeaponID( WEAPONID_KNIFES ) then 
-        UserCmd.viewangles = EulerAngles( UserCmd.viewangles.x, UserCmd.viewangles.y, 0 );
-    end;
-    
     if g_bRollAA:GetValue( ) then
         local local_player = entities.GetLocalPlayer( );
         if math.sqrt( local_player:GetPropFloat( "localdata", "m_vecVelocity[0]" ) ^ 2 + local_player:GetPropFloat( "localdata", "m_vecVelocity[1]" ) ^ 2 ) < 5 then
@@ -46,5 +34,4 @@ callbacks.Register( "CreateMove", "RollAA001", function( UserCmd )
             end;
         end;
     end;
-    print(UserCmd.viewangles)
 end );
